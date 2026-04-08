f713d897f4d969fa6d9910621ae2888289fc8e35
Murilo Beltrame <murilobeltrame@gmail.com>
2026-04-08 07:19:12 -0300

    Added Loggin support

--- src/Mediatr/Application.Mediatr/Shared/PipelineBehaviours/EnrichmentBehaviour.cs
+++ src/Mediatr/Application.Mediatr/Shared/PipelineBehaviours/EnrichmentBehaviour.cs
@@ -1,12 +1,16 @@
 ﻿
 using MediatR;
 
+using Microsoft.Extensions.Logging;
+
 namespace Application.Mediatr.Shared.PipelineBehaviours;
 
-public class EnrichmentBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : notnull
+public class EnrichmentBehaviour<TRequest, TResponse>(ILogger<EnrichmentBehaviour<TRequest, TResponse>> logger) : 
+    IPipelineBehavior<TRequest, TResponse> where TRequest : notnull
 {
     public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
     {
+        logger.LogDebug("Calling Enrichment Behaviour");
         return await next(cancellationToken);
     }
 }
--- src/Mediatr/Application.Mediatr/Shared/PipelineBehaviours/ValidationBehaviour.cs
+++ src/Mediatr/Application.Mediatr/Shared/PipelineBehaviours/ValidationBehaviour.cs
@@ -2,14 +2,20 @@
 
 using MediatR;
 
+using Microsoft.Extensions.Logging;
+
 namespace Application.Mediatr.Shared.PipelineBehaviours;
 
-public class ValidationBehaviour<TRequest, TResponse>(IEnumerable<IValidator<TRequest>> validators) :
+public class ValidationBehaviour<TRequest, TResponse>(
+    ILogger<ValidationBehaviour<TRequest,TResponse>> logger,
+    IEnumerable<IValidator<TRequest>> validators) :
     IPipelineBehavior<TRequest, TResponse>
     where TRequest : notnull
 {
     public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
     {
+        logger.LogDebug("Calling Validation Behaviour");
+        
         var context = new ValidationContext<TRequest>(request);
         var failures = await Task.WhenAll(validators.Select(v => v.ValidateAsync(context)));
         var errors = failures.SelectMany(f => f.Errors).Where(f => f != null).ToList();
